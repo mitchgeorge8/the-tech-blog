d97cb827a42c38157de118932c0dede96dd5d818
Mitchell George <mitchgeorge8@gmail.com>
2022-06-02 22:29:24 -0400

    Added log-in and sign-up templates and render routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
 
-// Get all posts
+// Render log-in page
+router.get("/log-in", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("log-in");
+});
+
+// Render sign-up page
+router.get("/sign-up", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("sign-up");
+});
+
+// Render homepage and get all posts
 router.get("/", (req, res) => {
   Post.findAll({
     include: [
@@ -23,6 +41,46 @@ router.get("/", (req, res) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       res.render("homepage", {
         posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Render single-post page and get single post
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        as: "comments",
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      res.render("single-post", {
+        post,
         loggedIn: req.session.loggedIn,
       });
     })
